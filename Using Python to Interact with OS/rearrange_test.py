8e106ba5e15ad50a6b68d0bce1e6d16282857c87
Cigir <67730788+ctemelkuran@users.noreply.github.com>
2020-12-12 15:00:34 +0300

    Update rearrange_test.py

--- Using Python to Interact with OS/rearrange_test.py
+++ Using Python to Interact with OS/rearrange_test.py
@@ -7,4 +7,19 @@ class TestRearrange(unittest.TestCase):
         expected = "Fatma Abaci"
         self.assertEqual(rearrange_name(testcase), expected) #assertEqual: both arguments are equal
 
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange_name(testcase),expected)
+
+    def test_double_name(self):
+        testcase = "Tolkein, John R. R."
+        expected = "John R. R. Tolkein"
+        self.assertEqual(rearrange_name(testcase),expected)
+
+    def test_one_name(self):
+        testcase = "Çığır"
+        expected = "Çığır"
+        self.assertEqual(rearrange_name(testcase),expected)
+
 unittest.main()
