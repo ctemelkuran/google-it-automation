de231d54c4e0471017cad2204ee0751a4cea1a4a
Cigir <67730788+ctemelkuran@users.noreply.github.com>
2020-12-12 16:38:21 +0300

    Update rearrange_test.py
    Added different assert methods.

--- Using Python to Interact with OS/rearrange_test.py
+++ Using Python to Interact with OS/rearrange_test.py
@@ -10,7 +10,7 @@ class TestRearrange(unittest.TestCase):
     def test_empty(self):
         testcase = ""
         expected = ""
-        self.assertEqual(rearrange_name(testcase),expected)
+        self.assertIs(rearrange_name(testcase),expected)
 
     def test_double_name(self):
         testcase = "Tolkein, John R. R."
@@ -20,6 +20,6 @@ class TestRearrange(unittest.TestCase):
     def test_one_name(self):
         testcase = "Çığır"
         expected = "Çığır"
-        self.assertEqual(rearrange_name(testcase),expected)
+        self.assertTrue(rearrange_name(testcase)==expected)
 
 unittest.main()
